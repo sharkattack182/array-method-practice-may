333f15e62280ac5c243a0520cdbcbfb68c246daf
Matthew Courtney <matthewdc182@gmail.com>
2021-05-11 13:49:58 -0400

    set up todays daily problem

--- practice.js
+++ practice.js
@@ -39,3 +39,24 @@ leftRotations("abcdef")
 
 rightRotations("abcdef") 
 // ➞ ["abcdef", "fabcde", "efabcd", "defabc", "cdefab", "bcdefa"]
+
+
+
+// Date: May 11 2020
+// Numbers in Strings
+// Create a function that takes an array of strings and returns an array with only the strings that have numbers in them. If there are no strings containing numbers, return an empty array.
+
+
+
+
+numInStr(["1a", "a", "2b", "b"]) 
+// ➞ ["1a", "2b"]
+
+numInStr(["abc", "abc10"]) 
+// ➞ ["abc10"]
+
+numInStr(["abc", "ab10c", "a10bc", "bcd"]) 
+// ➞ ["ab10c", "a10bc"]
+
+numInStr(["this is a test", "test1"]) 
+// ➞ ["test1"]
