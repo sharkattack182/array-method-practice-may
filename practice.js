9d28934f6ccd8e03c5794dea8e1bbcc92b19605e
Matthew Courtney <matthewdc182@gmail.com>
2021-05-14 15:58:32 -0400

    completed this in about ten minutes

--- practice.js
+++ practice.js
@@ -191,3 +191,46 @@ function reverseOdd(string) {
 // Date: May 14 2021
 // Positive Dominant
 // An array is positive dominant if it contains strictly more unique positive values than unique negative values. Write a function that returns true if an array is positive dominant.
+
+function isPositiveDominant(array) {
+    var sortedArr = array.slice().sort();
+    var results = [];
+    for (let i = 0; i < sortedArr.length; i++) {
+        const element = sortedArr[i];
+        if(element !== sortedArr[i + 1]) {
+            results.push(element)
+        }
+    }
+
+    var pos = 0;
+    var neg = 0;
+
+    results.forEach(value => {
+        if (value < 0) {
+            neg ++;
+        } else {
+            pos ++;
+        }
+    })
+
+    if(pos > neg) {
+        console.log("True")
+    } else {
+        console.log("False")
+    }
+}
+
+
+isPositiveDominant([1, 1, 1, 1, -3, -4]) 
+// ➞ false
+// There is only 1 unique positive value (1).
+// There are 2 unique negative values (-3, -4).
+
+isPositiveDominant([5, 99, 832, -3, -4]) 
+// ➞ true
+
+isPositiveDominant([5, 0]) 
+// ➞ true
+
+isPositiveDominant([0, -4, -1]) 
+// ➞ false
