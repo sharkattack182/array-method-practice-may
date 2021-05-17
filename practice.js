a10046c66598a24ad73272671f7d177b52d72fb1
Matthew Courtney <matthewdc182@gmail.com>
2021-05-16 22:48:37 -0400

    added om another problem

--- practice.js
+++ practice.js
@@ -512,11 +512,35 @@ function countBoomerangs(array) {
 }
 
 
-countBoomerangs([9, 5, 9, 5, 1, 1, 1]) 
+// countBoomerangs([9, 5, 9, 5, 1, 1, 1]) 
 // ➞ 2
 
-countBoomerangs([5, 6, 6, 7, 6, 3, 9]) 
+// countBoomerangs([5, 6, 6, 7, 6, 3, 9]) 
 // ➞ 1
 
-countBoomerangs([4, 4, 4, 9, 9, 9, 9]) 
+// countBoomerangs([4, 4, 4, 9, 9, 9, 9]) 
 // ➞ 0
+
+
+
+
+// Date: May 16 2021
+// Parseltongue
+// Hermione has come up with a precise formula for determining whether or not a phrase was ssspoken by a parssseltongue (a reference from the Harry Potter universe; the language of ssserpents and those who can converse with them).
+// Each word in a sssentence must contain either:
+// Two or more consecutive instances of the letter "s" (i.e. must be together ss..), or...
+// Zero instances of the letter "s" by itself.
+
+isParselTongue("Sshe ssselects to eat that apple. ") 
+// ➞ true
+
+isParselTongue("She ssselects to eat that apple. ") 
+// ➞ false
+// "She" only contains one "s".
+
+isParselTongue("Beatrice samples lemonade") 
+// ➞ false
+// While "samples" has 2 instances of "s", they are not together.
+
+isParselTongue("You ssseldom sssspeak sso boldly, ssso messmerizingly.") 
+// ➞ true
