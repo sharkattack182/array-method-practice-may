f59857e329ba004f4244d4c8bdcf7b666bbca4a3
Matthew Courtney <matthewdc182@gmail.com>
2021-05-17 17:04:43 -0400

    pretty simple just filtered the array using a regex test to determine if the word included any number

--- practice.js
+++ practice.js
@@ -527,3 +527,24 @@ function countBoomerangs(array) {
 // Date: MAy 17 2020
 // Numbers In Strings
 // Create a function that takes an array of strings and returns an array with only the strings that have numbers in them. If there are no strings containing numbers, return an empty array.
+
+function numInStr(array) {
+    var returnArr = array.filter(word => 
+        /\d/.test(word)
+    )
+
+    console.log(returnArr)
+}
+
+
+numInStr(["1a", "a", "2b", "b"]) 
+// ➞ ["1a", "2b"]
+
+numInStr(["abc", "abc10"]) 
+// ➞ ["abc10"]
+
+numInStr(["abc", "ab10c", "a10bc", "bcd"]) 
+// ➞ ["ab10c", "a10bc"]
+
+numInStr(["this is a test", "test1"]) 
+// ➞ ["test1"]
