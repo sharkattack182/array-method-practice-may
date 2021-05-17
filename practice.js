7ae5cc89ef8249c70f6f9f356674b5405e36f798
Matthew Courtney <matthewdc182@gmail.com>
2021-05-17 16:26:07 -0400

    added in a ranodm problem seems pretty simple

--- practice.js
+++ practice.js
@@ -524,23 +524,6 @@ function countBoomerangs(array) {
 
 
 
-// Date: May 16 2021
-// Parseltongue
-// Hermione has come up with a precise formula for determining whether or not a phrase was ssspoken by a parssseltongue (a reference from the Harry Potter universe; the language of ssserpents and those who can converse with them).
-// Each word in a sssentence must contain either:
-// Two or more consecutive instances of the letter "s" (i.e. must be together ss..), or...
-// Zero instances of the letter "s" by itself.
-
-isParselTongue("Sshe ssselects to eat that apple. ") 
-// ➞ true
-
-isParselTongue("She ssselects to eat that apple. ") 
-// ➞ false
-// "She" only contains one "s".
-
-isParselTongue("Beatrice samples lemonade") 
-// ➞ false
-// While "samples" has 2 instances of "s", they are not together.
-
-isParselTongue("You ssseldom sssspeak sso boldly, ssso messmerizingly.") 
-// ➞ true
+// Date: MAy 17 2020
+// Numbers In Strings
+// Create a function that takes an array of strings and returns an array with only the strings that have numbers in them. If there are no strings containing numbers, return an empty array.
