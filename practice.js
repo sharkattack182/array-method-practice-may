1613c4bb8b9a05304c94b7965199616a4aa0b3e9
Matthew Courtney <matthewdc182@gmail.com>
2021-05-17 17:25:32 -0400

    completed in like five minuted good practice uing the reduce method

--- practice.js
+++ practice.js
@@ -553,7 +553,16 @@ function numInStr(array) {
 // Oddish vs Evenish;
 // Create a function that determines whether a number is Oddish or Evenish. A number is Oddish if the sum of all of its digits is odd, and a number is Evenish if the sum of all of its digits is even. If a number is Oddish, return "Oddish". Otherwise, return "Evenish".
 
-
+function oddishOrEvenish(num) {
+    const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue)
+    var array = num.toString().split("");
+    var total = array.reduce(reducer);
+    if(total % 2 === 0) {
+        console.log("Evenish")
+    } else {
+        console.log("Oddish")
+    }
+}
 
 oddishOrEvenish(43) 
 // ➞ "Oddish"
