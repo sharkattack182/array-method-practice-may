9c86541f69ebcc384c14644465acb26a766ec464
Matthew Courtney <matthewdc182@gmail.com>
2021-05-24 21:54:33 -0400

    added in a daily problem

--- practice.js
+++ practice.js
@@ -959,3 +959,21 @@ function sumRound(num) {
 
 // sumRound(54210) 
 // ➞ "10 200 4000 50000"
+
+
+
+// Date: May 24 2021
+// Sort by Digit Length, then By Number Itself
+// Write a function that sorts an array of integers by their digit length in descending order, then settles ties by sorting numbers with the same digit length in ascending order.
+
+
+
+
+digitSort([77, 23, 5, 7, 101])
+// ➞ [101, 23, 77, 5, 7]
+
+digitSort([1, 5, 9, 2, 789, 563, 444])
+// ➞ [444, 563, 789, 1, 2, 5, 9]
+
+digitSort([53219, 3772, 564, 32, 1])
+// ➞ [53219, 3772, 564, 32, 1]
